a4484a2ff92d56a1ae2c145280ce57273a5b85ff
3048 <olena99y@gmail.com>
2021-09-02 16:42:38 +0300

    added intensitive parameter to joi trip validation

--- validators/trip_validator.js
+++ validators/trip_validator.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 const countriesEnum = require('../configs/countries.enum');
 
 const createTripValidator = Joi.object({
-  country: Joi.string().alphanum().valid(...Object.values(countriesEnum)).required(),
+  country: Joi.string().alphanum().valid(...Object.values(countriesEnum)).insensitive()
+    .required(),
 
   price: Joi.number().positive().required(),
 
